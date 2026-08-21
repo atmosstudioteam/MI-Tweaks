2320d2c3f05156e9d924ae22b438987f9c7e062c
Atmos Studio <atmos.studio.team@gmail.com>
2026-08-21 19:47:33 +0200

    Update MITweaksClient.java
    Disable custom machine blueprint renderer

--- src/main/java/net/swedz/mi_tweaks/MITweaksClient.java
+++ src/main/java/net/swedz/mi_tweaks/MITweaksClient.java
@@ -5,10 +5,8 @@ import aztech.modern_industrialization.client.machines.multiblocks.MultiblockMac
 import aztech.modern_industrialization.machines.MachineBlock;
 import aztech.modern_industrialization.machines.MachineBlockEntity;
 import aztech.modern_industrialization.machines.multiblocks.MultiblockMachineBlockEntity;
-import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
-import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.neoforged.api.distmarker.Dist;
@@ -19,14 +17,9 @@ import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.fml.event.lifecycle.FMLConstructModEvent;
 import net.neoforged.neoforge.client.event.ClientPlayerNetworkEvent;
-import net.neoforged.neoforge.client.event.ModelEvent;
-import net.neoforged.neoforge.client.extensions.common.IClientItemExtensions;
-import net.neoforged.neoforge.client.extensions.common.RegisterClientExtensionsEvent;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.swedz.mi_tweaks.compat.mi.custom.MITweaksMIRegistries;
-import net.swedz.mi_tweaks.item.MachineBlueprintItem;
-import net.swedz.mi_tweaks.item.renderer.BlockOverlayingItemRenderer;
 
 import java.util.stream.Stream;
 
@@ -34,44 +27,35 @@ import java.util.stream.Stream;
 @EventBusSubscriber(modid = MITweaks.ID, value = Dist.CLIENT)
 public final class MITweaksClient
 {
-	private static final ModelResourceLocation RAW_ITEM_MODEL_LOCATION = ModelResourceLocation.standalone(MITweaks.id("item/machine_blueprint_raw"));
-	
 	public MITweaksClient(IEventBus bus)
 	{
-		bus.addListener(RegisterClientExtensionsEvent.class, (event) ->
-				event.registerItem(
-						new IClientItemExtensions()
-						{
-							@Override
-							public BlockEntityWithoutLevelRenderer getCustomRenderer()
-							{
-								return new BlockOverlayingItemRenderer(RAW_ITEM_MODEL_LOCATION, MachineBlueprintItem::getMachineBlock);
-							}
-						},
-						MITweaksItems.MACHINE_BLUEPRINT.asItem()
-				));
 	}
-	
+
 	@SubscribeEvent
 	private static void registerBlockEntityRenderers(FMLClientSetupEvent event)
 	{
-		for(DeferredHolder<Block, ? extends Block> blockDef : Stream.concat(MITweaksBlocks.Registry.BLOCKS.getEntries().stream(), MITweaksMIRegistries.BLOCKS.getEntries().stream()).toList())
+		for(DeferredHolder<Block, ? extends Block> blockDef :
+				Stream.concat(
+						MITweaksBlocks.Registry.BLOCKS.getEntries().stream(),
+						MITweaksMIRegistries.BLOCKS.getEntries().stream()
+				).toList())
 		{
 			if(blockDef.get() instanceof MachineBlock machine)
 			{
 				MachineBlockEntity blockEntity = machine.getBlockEntityInstance();
 				BlockEntityType type = blockEntity.getType();
-				
+
 				BlockEntityRendererProvider provider = switch (blockEntity)
 				{
 					case MultiblockMachineBlockEntity be -> MultiblockMachineBER::new;
 					default -> MachineBlockEntityRenderer::new;
 				};
+
 				BlockEntityRenderers.register(type, provider);
 			}
 		}
 	}
-	
+
 	@SubscribeEvent
 	private static void init(FMLConstructModEvent __)
 	{
@@ -82,14 +66,11 @@ public final class MITweaksClient
 				MITweaks.config().machineBlueprints().required().placing().isLearning() ||
 				MITweaks.config().machineBlueprints().required().renderingHatches().isLearning()))
 			{
-				event.getPlayer().displayClientMessage(MITweaks.text().learningDisabledButRequiringLearning(), false);
+				event.getPlayer().displayClientMessage(
+						MITweaks.text().learningDisabledButRequiringLearning(),
+						false
+				);
 			}
 		});
-	}
-	
-	@SubscribeEvent
-	private static void onModifyBakingResult(ModelEvent.RegisterAdditional event)
-	{
-		event.register(RAW_ITEM_MODEL_LOCATION);
 	}
 }
